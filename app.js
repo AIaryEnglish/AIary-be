6f418df5cdb05dfaea361c66971e65037f7fe207
Hyeonjin <oboki2001@naver.com>
2025-08-19 20:15:52 +0900

    Feat: Express 서버 설정 개선 (dotenv 로드, static public 폴더 추가)

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const indexRouter = require("./routes/index")
-const cors = require("cors");
 require("dotenv").config();
-const mongoURI = process.env.MONGODB_URI_PROD;
 
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+
+const mongoURI = process.env.MONGODB_URI_PROD;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json()); // req.body가 객체로 인식됨
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/api", indexRouter)
+app.use("/api", indexRouter);
+app.use(express.static(path.join(__dirname, "public"))); // public 폴더 정적 서빙
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log("mongoose connected"))
   .catch((err) => console.log("DB connection fail", err));
 
-  app.listen(process.env.PORT || 5000, ()=>{
-    console.log("server on")
-  })
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server on");
+});
