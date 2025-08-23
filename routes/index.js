f194a16d977e66b58eb7d71523872686fcea53d5
Sara han <hans63237@gmail.com>
2025-08-23 09:08:03 +0900

    feat: 유저 생성 로직 추가

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const diaryApi = require("./diary.api");
+const userApi = require("./user.api");
 
 router.use("/diary", diaryApi);
+router.use("/user", userApi);
 
 module.exports = router;
