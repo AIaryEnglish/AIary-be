6ca656996416ef98d440e1ffd5b091b65c2d0aba
Hyeonjin <oboki2001@naver.com>
2025-08-24 17:49:33 +0900

    Fix: 오타 수정(newDate → new Date)

--- utils/time.js
+++ utils/time.js
@@ -11,7 +11,7 @@ function kstDateKey(date = new Date()) {
 // 오늘로부터 n일 전의 날짜를 KST 기준 "YYYY-MM-DD" 문자열로 변환
 function kstDateKeyMinusDays(days) {
   const ms = days * 24 * 60 * 60 * 1000;
-  return kstDateKey(newDate(Date.now() - ms));
+  return kstDateKey(new Date(Date.now() - ms));
 }
 
 module.exports = { kstDateKey, kstDateKeyMinusDays, ZONE };
