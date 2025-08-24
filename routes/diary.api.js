0c0965b689998f8838a4c0bd72195429a5a7a691
Hyeonjin <oboki2001@naver.com>
2025-08-24 17:47:38 +0900

    Feat: 특정 사용자의 일기 조회 라우트 추가 및 인증 연동

--- routes/diary.api.js
+++ routes/diary.api.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const diaryController = require("../controllers/diary.controller");
+const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.get("/", diaryController.getAllDiaries);
+router.get(
+  "/my",
+  authController.authenticate,
+  diaryController.getOneUserDiaries
+);
+router.get(
+  "/my/date",
+  authController.authenticate,
+  diaryController.getDiaryByDate
+);
 
 module.exports = router;
