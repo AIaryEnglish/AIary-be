8df2540fd1bfe7edcaa5fc980206a91cc7fd37f7
Sey <ppresi14789@gmail.com>
2025-08-24 11:41:38 +0900

    feat: 단어장 삭제 및 업데이트(단어 상태)

--- controllers/vocab.controller.js
+++ controllers/vocab.controller.js
@@ -7,10 +7,41 @@ const vocabController = {};
 // 특정 다이어리의 단어 목록 가져오기
 vocabController.getAllWords = async (req, res) => {
   try {
-    const vocabList = await Vocabook.find({ diaryId: req.params.diaryId }).sort(
-      { createdAt: -1 }
-    );
+    const vocabList = await Vocabook.find({
+      diaryId: req.params.diaryId,
+      isDeleated: false,
+    }).sort({ createdAt: -1 });
     res.json(vocabList);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+vocabController.toggleStatus = async (req, res) => {
+  try {
+    const vocab = await Vocabook.findById(req.params.id);
+    if (!vocab) return res.status(404).json({ message: "Not found" });
+
+    vocab.status = vocab.status === "learning" ? "mastered" : "learning";
+    await vocab.save();
+
+    res.json(vocab);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+vocabController.deleteWord = async (req, res) => {
+  try {
+    console.log("Delete called for id:", req.params.id);
+    const vocab = await Vocabook.findByIdAndUpdate(
+      req.params.id,
+      { isDeleated: true }, // 실제 삭제 X
+      { new: true }
+    );
+    if (!vocab) return res.status(404).json({ message: "Not found" });
+
+    res.json({ message: "Deleted successfully" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
--- routes/vocab.api.js
+++ routes/vocab.api.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const vocabController = require("../controllers/vocab.controller");
 
 router.get("/:diaryId", vocabController.getAllWords);
+router.post("/:id", vocabController.toggleStatus);
+router.delete("/:id", vocabController.deleteWord);
 
 module.exports = router;
